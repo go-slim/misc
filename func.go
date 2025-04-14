b649bb8d1b392a665d4d8182f62fcfeef039fb70
maofeng <hupeh@qq.com>
2025-04-15 01:41:31 +0800

    feat:  新增 MustCall 与 MustCall With 函数

--- func.go
+++ func.go
@@ -1,5 +1,12 @@
 package misc
 
+import (
+	"reflect"
+	"runtime"
+
+	"zestack.dev/l4g"
+)
+
 func Wrap(fns ...func() error) func() error {
 	return func() error {
 		return Call(fns...)
@@ -30,4 +37,24 @@ func CallWith[T any](val T, fns ...func(T) error) error {
 		}
 	}
 	return nil
+}
+
+func MustCall(fns ...func() error) {
+	for _, fn := range fns {
+		if err := fn(); err != nil {
+			ptr := reflect.ValueOf(fn).Pointer()
+			fi := runtime.FuncForPC(ptr)
+			l4g.Fatalf("%s failed: %v", fi.Name(), err)
+		}
+	}
+}
+
+func MustCallWith[T any](val T, fns ...func(T) error) {
+	for _, fn := range fns {
+		if err := fn(val); err != nil {
+			ptr := reflect.ValueOf(fn).Pointer()
+			fi := runtime.FuncForPC(ptr)
+			l4g.Fatalf("%s(ctx) failed: %v", fi.Name(), err)
+		}
+	}
 }
