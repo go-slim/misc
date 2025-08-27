cb7de5a39493a9261d863b065c9a37ffdea73144
maofeng <hupeh@qq.com>
2025-08-27 12:00:22 +0800

    test(misc): add benchmarks for tmpl/interpolate/digest/unsafe/IsZero

---
+++ bench_test.go
@@ -0,0 +1,113 @@
+package misc
+
+import (
+    "io"
+    "strings"
+    "testing"
+)
+
+func BenchmarkStrtr(b *testing.B) {
+    in := "Hello, {name}! Today is {day}. {name} likes {thing}."
+    m := map[string]any{"name": "Alice", "day": "Wednesday", "thing": "Go"}
+    b.ReportAllocs()
+    for i := 0; i < b.N; i++ {
+        _, _ = Strtr(in, m)
+    }
+}
+
+func BenchmarkTmpl(b *testing.B) {
+    tpl := "/users/{ID}/posts/{Post}?q={Q}"
+    data := map[string]any{"ID": 123, "Post": 456, "Q": "search"}
+    b.ReportAllocs()
+    for i := 0; i < b.N; i++ {
+        _, _ = Tmpl(tpl, "{", "}", data)
+    }
+}
+
+func BenchmarkInterpolate_TagFunc(b *testing.B) {
+    tpl := "Hi, {name}! You have {n} messages."
+    vars := map[string]string{"name": "alice", "n": "7"}
+    fn := TagFunc(func(w io.Writer, tag string) (int, error) {
+        if v, ok := vars[tag]; ok {
+            // simulate some work
+            return w.Write([]byte(strings.ToUpper(v)))
+        }
+        return w.Write([]byte(""))
+    })
+    b.ReportAllocs()
+    for i := 0; i < b.N; i++ {
+        _, _ = Interpolate(tpl, "{", "}", &strings.Builder{}, fn)
+    }
+}
+
+func BenchmarkMD5(b *testing.B) {
+    s := strings.Repeat("x", 1024)
+    b.ReportAllocs()
+    for i := 0; i < b.N; i++ {
+        _ = MD5(s)
+    }
+}
+
+func BenchmarkSha1(b *testing.B) {
+    s := strings.Repeat("x", 1024)
+    b.ReportAllocs()
+    for i := 0; i < b.N; i++ {
+        _ = Sha1(s)
+    }
+}
+
+func BenchmarkSha256(b *testing.B) {
+    s := strings.Repeat("x", 1024)
+    b.ReportAllocs()
+    for i := 0; i < b.N; i++ {
+        _ = Sha256(s)
+    }
+}
+
+func BenchmarkPasswordHashVerify(b *testing.B) {
+    pwd := "S3cret!"
+    hash, _ := PasswordHash(pwd)
+
+    b.Run("hash", func(b *testing.B) {
+        b.ReportAllocs()
+        for i := 0; i < b.N; i++ {
+            _, _ = PasswordHash(pwd)
+        }
+    })
+
+    b.Run("verify", func(b *testing.B) {
+        b.ReportAllocs()
+        for i := 0; i < b.N; i++ {
+            _ = PasswordVerify(pwd, hash)
+        }
+    })
+}
+
+func BenchmarkUnsafeConversions(b *testing.B) {
+    b.Run("bytes_to_string", func(b *testing.B) {
+        bs := []byte(strings.Repeat("a", 64))
+        b.ReportAllocs()
+        for i := 0; i < b.N; i++ {
+            _ = UnsafeBytesToString(bs)
+        }
+    })
+
+    b.Run("string_to_bytes", func(b *testing.B) {
+        s := strings.Repeat("a", 64)
+        b.ReportAllocs()
+        for i := 0; i < b.N; i++ {
+            _ = UnsafeStringToBytes(s)
+        }
+    })
+}
+
+func BenchmarkIsZero(b *testing.B) {
+    type T struct{ N int }
+    cases := []any{0, "", (*T)(nil), T{}, []int(nil)}
+    b.ReportAllocs()
+    for i := 0; i < b.N; i++ {
+        for _, v := range cases {
+            _ = IsZero(v)
+        }
+    }
+}
